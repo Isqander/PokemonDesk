5acb965d1f97fe1f7b5e8b795a7fdd0f14c64a4e
isqander@gmail.com <Lorein1!>
2020-11-03 19:04:41 +0300

    HomeWork 3 first video

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import './custom.css';
-import style from './App.modules.scss';
+import style from './App.module.scss';
 const App = () => {
     return (
         <div className={cn(style.header, 'color')}>
